bf817326f1cfc0d2841e857f4bc452d3a65eebf1
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2023-03-23 10:18:55 +0100

    tror att jag slänger sql_select funktionen

--- app.rb
+++ app.rb
@@ -2,81 +2,192 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
+require 'bcrypt'
 
-get('/')  do
-  db = SQLite3::Database.new("db/q.db")
+enable :sessions
+
+def conn(path)
+  db = SQLite3::Database.new(path)
   db.results_as_hash = true
-  
-  random = db.execute("SELECT * FROM quizzes INNER JOIN quizzes_owners ON quizzes.id = quizzes_owners.quiz_id WHERE quizzes_owners.user_id = ? ORDER BY RANDOM() LIMIT 5")
-  often = db.execute("SELECT * FROM quizzes INNER JOIN quizzes_owners ON quizzes.id = quizzes_owners.quiz_id WHERE quizzes_owners.user_id = ? AND ORDER BY (SELECT COUNT(*) FROM quizzes_completions WHERE quiz_id = quizzes.id AND date > ? LIMIT 1) DESC LIMIT 5")
-  popular = db.execute("SELECT * FROM quizzes ORDER BY (SELECT COUNT(*) quizzes_likes WHERE quizzes_likes.quiz_id = quizzes.id LIMIT 1) DESC LIMIT 5")
-
-  slim(:"index", locals: {random:random, often:often, popular:popular})
+  return db
 end
 
-get('/quizzes') do
-
+def sql_select(db, columns, from, join, where, values, order, limit)
+  sql = "SELECT " + columns + " FROM " + from + " " + join + " WHERE " + where + " ORDER BY " + order + " LIMIT " + limit
+  puts "Query: #{sql}"
+  return db.execute(sql, *values)
 end
 
-get('/user/:id') do
-
+get('/') do
+  if(session[:user_id] == nil)
+    redirect('/forms')
+  else
+    db = conn("db/q.db")
+    join = "INNER JOIN quizzes_owners ON quizzes_owners.quiz_id = quizzes.id"
+    quizzes = sql_select(db, "*", "quizzes", join, "quizzes_owners.user_id = ?", [session[:user_id].to_s], "id DESC", "10")
+    puts "Result: #{quizzes}"
+    db.close
+    slim(:index, locals:{quizzes:quizzes, session:session})
+  end
 end
 
-get('/user/:id/quizzes') do
+get('/forms') do
+  slim(:forms)
+end
 
+post('/signup') do
+  name = params[:name]
+  uid = params[:uid]
+  pwd = params[:pwd]
+  pwd_confirm = params[:pwd_confirm]
+
+  if pwd == pwd_confirm
+
+    pwdigest = BCrypt::Password.create(pwd)
+
+    db = conn("db/q.db")
+    db.execute('INSERT INTO users (name, uid, pwdigest) VALUES (?, ?, ?)', name, uid, pwdigest)
+    db.close
+
+    redirect('/')
+  else
+    puts "Lösenorden matchar inte."
+    redirect('/forms?error=pwdmatch')
+  end
+end
+
+post('/login') do
+  if params[:uid] == nil || params[:pwd] == nil
+    puts "Fel användarnamn."
+    redirect('/forms?error=wrongusername')
+  end
+
+  uid = params[:uid]
+  pwd = params[:pwd]
+
+  db = conn("db/q.db")
+  result = db.execute('SELECT * FROM users WHERE uid = ?', uid).first
+  db.close
+
+  if result
+
+    pwdigest = result['pwdigest']
+
+    if BCrypt::Password.new(pwdigest) == pwd
+      session[:user_id] = result['id']
+      session[:user_uid] = result['uid']
+      redirect('/')
+    else
+      puts "Fel lösenord."
+      redirect('/forms?error=wrongpwd')
+    end
+  else
+    puts "Fel användarnamn."
+    redirect('/forms?error=wrongusername')
+  end
+end
+
+post('/logout') do
+  session[:user_id] = nil
+  redirect('/')
 end
 
 get('/quiz/new') do
-#   slim(:"albums/new")
+  slim(:"quiz/new")
 end
 
-post('/quiz/create') do
-#   db = SQLite3::Database.new("db/chinook-crud.db")
+get('/quiz/new') do
+  slim(:"quiz/new")
+end
 
-#   title = params[:title]
-#   artist_id = params[:artist_id]
-
-#   db.execute("INSERT INTO albums (Title, ArtistId) VALUES (?,?)",title,artist_id)
-
-#   redirect('/albums')
+post('/quiz') do # create
+  redirect('/')
 end
 
 get('/quiz/:id') do
-#   id = params[:id].to_i
-#   db = SQLite3::Database.new("db/chinook-crud.db")
-#   db.results_as_hash = true
+  id = params[:id]
 
-#   result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
-#   result2 = db.execute("SELECT Name AS ArtistName FROM artists WHERE ArtistId IN (SELECT ArtistId FROM albums WHERE AlbumId = ?)",id).first
-  
-#   slim(:"albums/show",locals:{result:result,result2:result2})
-end
+  db = conn("db/q.db")
+  quiz = sql_select(db, "*", "quizzes", "", "quizzes.id = ?", [id.to_s], "id DESC", "1")
+  owners = sql_select(db, "*", "quizzes_owners", "", "quizzes_owners.quiz_id = ?", [id.to_s], "id DESC", "")
+  puts "Result: #{quizzes}"
+  db.close
 
-post('/quiz/:id/delete') do
-#   id = params[:id].to_i
-#   db = SQLite3::Database.new("db/chinook-crud.db")
+  case quiz[]
+  access = 
 
-#   db.execute("DELETE FROM albums WHERE AlbumId = ?",id)
-  
-#   redirect('/albums')
+  slim(:"quiz/index", locals:{id:id})
 end
 
 get('/quiz/:id/edit') do
-#   id = params[:id].to_i
-#   db = SQLite3::Database.new("db/chinook-crud.db")
-#   db.results_as_hash = true
-
-#   result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
-
-#   slim(:"albums/edit",locals:{result:result})
+  slim(:"quiz/index/edit")
 end
 
-post('/quiz/:id/update') do
-#   id = params[:id].to_i
-#   title = params[:title]
-#   db = SQLite3::Database.new("db/chinook-crud.db")
+post('/quiz/:id/update') do # används för både update & delete, inklusive add collaborator
+  redirect('/')
+end
 
-#   db.execute("UPDATE albums SET Title = ? WHERE AlbumId = ?",title,id)
+# get('/quizzes') do
+
+# end
+
+# get('/user/:id') do
+
+# end
+
+# get('/user/:id/quizzes') do
+
+# end
+
+
+
+# post('/quiz/create') do
+# #   db = SQLite3::Database.new("db/chinook-crud.db")
+
+# #   title = params[:title]
+# #   artist_id = params[:artist_id]
+
+# #   db.execute("INSERT INTO albums (Title, ArtistId) VALUES (?,?)",title,artist_id)
+
+# #   redirect('/albums')
+# end
+
+# get('/quiz/:id') do
+# #   id = params[:id].to_i
+# #   db = SQLite3::Database.new("db/chinook-crud.db")
+# #   db.results_as_hash = true
+
+# #   result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
+# #   result2 = db.execute("SELECT Name AS ArtistName FROM artists WHERE ArtistId IN (SELECT ArtistId FROM albums WHERE AlbumId = ?)",id).first
   
-#   redirect('/albums')
-end
+# #   slim(:"albums/show",locals:{result:result,result2:result2})
+# end
+
+# post('/quiz/:id/delete') do
+# #   id = params[:id].to_i
+# #   db = SQLite3::Database.new("db/chinook-crud.db")
+
+# #   db.execute("DELETE FROM albums WHERE AlbumId = ?",id)
+  
+# #   redirect('/albums')
+# end
+
+# get('/quiz/:id/edit') do
+# #   id = params[:id].to_i
+# #   db = SQLite3::Database.new("db/chinook-crud.db")
+# #   db.results_as_hash = true
+
+# #   result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
+
+# #   slim(:"albums/edit",locals:{result:result})
+# end
+
+# post('/quiz/:id/update') do
+# #   id = params[:id].to_i
+# #   title = params[:title]
+# #   db = SQLite3::Database.new("db/chinook-crud.db")
+
+# #   db.execute("UPDATE albums SET Title = ? WHERE AlbumId = ?",title,id)
+  
+# #   redirect('/albums')
+# end
