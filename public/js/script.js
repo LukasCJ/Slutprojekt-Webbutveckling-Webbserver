396eb3ad98888223adcaa8cee9efb9cda58bc90f
LukasCJ <93259361+LukasCJ@users.noreply.github.com>
2023-04-07 16:47:35 +0200

    man kan välja och lägga till frågor i skapa menyn

--- public/js/script.js
+++ public/js/script.js
@@ -39,29 +39,71 @@ $('section#create .question .button.delete, section#create .answer .button.delet
 });
 
 $('section#create .button.add_question').click(function() {
-    console.log('hey');
-    let question_numbers = []
-    $('.content_container .question, .content_container .answer').each(function() {
-        question_numbers.push($(this).data('local-id'));
-        console.log(question_numbers); 
-    });
-    let hi = Math.max(...question_numbers);
-    if(Number.isInteger(hi)) {
-        var id = hi + 1;
+    var item, item_q_id, q_id;
+    if($('.question.focus').length == 1) {
+        item = $('.question.focus');
+        item_q_id = parseInt(item.data('q-id'));
+        q_id = item_q_id+1;
+        if($('.answer[data-q-id="'+item_q_id+'"]').length > 0) {
+            $('.answer[data-q-id="'+item_q_id+'"]').last().after('<div class="question" data-q-id="' + q_id + '"><p class="num">#' + q_id + '</p><input placeholder="Fråga" type="text" /><button class="button delete" type="button">Radera</button></div>');
+        } else {
+            item.after('<div class="question" data-q-id="' + q_id + '"><p class="num">#' + q_id + '</p><input placeholder="Fråga" type="text" /><button class="button delete" type="button">Radera</button></div>')
+        }
+    } else if($('.answer.focus').length == 1) {
+        item = $('.answer.focus');
+        item_q_id = parseInt(item.data('q-id'));
+        q_id = item_q_id+1;
+        $('.answer[data-q-id="'+item_q_id+'"]').last().after('<div class="question" data-q-id="' + q_id + '"><p class="num">#' + q_id + '</p><input placeholder="Fråga" type="text" /><button class="button delete" type="button">Radera</button></div>');
     } else {
-        var id = Math.ceil(hi);
+        var hi = 1;
+        $('.question, .answer').each(function() {
+            item_q_id = parseInt($(this).data('q-id'));
+            console.log(item_q_id);
+            if(item_q_id > hi) {
+                hi = item_q_id;
+                console.log(hi);
+            }
+        });
+        q_id = hi+1;
+        $('.content_container').append('<div class="question" data-q-id="' + q_id + '"><p class="num">#' + q_id + '</p><input placeholder="Fråga" type="text" /><button class="button delete" type="button">Radera</button></div>');
     }
-    $('.content_container').append('<div class="question" data-local-id="' + id + '"><p class="num">#' + id + '</p><input placeholder="Fråga" type="text" /><button class="button delete" type="button">Radera</button></div>');
 });
 
-$('section#create .question, section#create .answer').click(function() {
-    if(!$(this).hasClass('focus')) {
-        $('.content_container .question, .content_container .answer').each(function() {
-            $(this).removeClass('focus');
-        });
+// $('section#create .button.add_answer').click(function() {
+
+// item = $('.answer.focus');
+// item_q_id = parseInt(item.data('q-id'));
+// item_a_id = parseInt(item.data('a-id'));
+// a_id = item_a_id+1;
+// item.after('<div class="question" data-local-id="' + q_id + '"><p class="num">#' + q_id + '</p><input placeholder="Fråga" type="text" /><button class="button delete" type="button">Radera</button></div>');
+
+//     console.log('hey');
+//     let question_numbers = []
+//     $('.content_container .question, .content_container .answer').each(function() {
+//         question_numbers.push($(this).data('local-id'));
+//         console.log(question_numbers); 
+//     });
+//     let hi = Math.max(...question_numbers);
+//     if(Number.isInteger(hi)) {
+//         var id = hi + 1;
+//     } else {
+//         var id = Math.ceil(hi);
+//     }
+//     $('.content_container').append('<div class="question" data-local-id="' + id + '"><p class="num">#' + id + '</p><input placeholder="Fråga" type="text" /><button class="button delete" type="button">Radera</button></div>');
+// });
+
+});
+
+$('section#create .content_container').on('click', '.question, .answer, .question *, .answer *', function() {
+    console.log($(this));
+    if(!$(this).hasClass('question') && !$(this).hasClass('answer')) {
+        var item = $(this).parents('.question, .answer');
+        if(!item.hasClass('focus')) {
+            $('.question, .answer').removeClass('focus');
+            item.addClass('focus');
+        }
+    } else if(!$(this).hasClass('focus')) {
+        $('.question, .answer').removeClass('focus');
         $(this).addClass('focus');
     }
 });
-
-});
-
